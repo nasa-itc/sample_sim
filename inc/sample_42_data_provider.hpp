5c6e23d97cf5c0c4397e166e078e80161ef4a090
msuder <mark.d.suder@nasa.gov>
2020-09-24 14:05:37 -0400

    #3 - Cleaned up numerous parts of the code to make the sample more explicit and realistic.  Added numerous comments to describe how a user would change/not change the sample to suit their needs.

--- inc/sample_42_data_provider.hpp
+++ inc/sample_42_data_provider.hpp
@@ -1,31 +1,27 @@
 #ifndef NOS3_SAMPLE42DATAPROVIDER_HPP
 #define NOS3_SAMPLE42DATAPROVIDER_HPP
 
-// Library Includes
-#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
-// NOS Engine Includes
-#include <ItcLogger/Logger.hpp>
-
-// Simulator Includes
-#include <sample_data_point.hpp>
 #include <sim_data_42socket_provider.hpp>
 
 namespace Nos3
 {
+    // vvv This is pretty standard for a 42 data provider (if one is needed for your sim)
     class Sample42DataProvider : public SimData42SocketProvider
     {
     public:
-        // Constructors
+        // Constructors / destructor
         Sample42DataProvider(const boost::property_tree::ptree& config);
-
-        // Deconstructors
-        ~Sample42DataProvider(void);
 
         // Accessors
         boost::shared_ptr<SimIDataPoint> get_data_point(void) const;
 
     private:
+        // Disallow these
+        ~Sample42DataProvider(void) {};
+        Sample42DataProvider& operator=(const Sample42DataProvider&) {};
+
         int16_t _sc;  // Which spacecraft number to parse out of 42 data
     };
 }
--- inc/sample_data_point.hpp
+++ inc/sample_data_point.hpp
@@ -1,36 +1,37 @@
 #ifndef NOS3_SAMPLEDATAPOINT_HPP
 #define NOS3_SAMPLEDATAPOINT_HPP
 
-// Library Includes
 #include <boost/shared_ptr.hpp>
-#include <string>
 
-// NOS Engine Includes
-#include <ItcLogger/Logger.hpp>
-
-// Simulator Includes
-#include <sim_i_data_point.hpp>
-#include <sim_data_42socket_provider.hpp>
+#include <sim_42data_point.hpp>
 
 namespace Nos3
 {
+    // vvv This is pretty standard for a data point (if one is needed to transfer data between a data provider and a hardware model for your sim)
     class SampleDataPoint : public SimIDataPoint
     {
     public:
-        // Constructors
-        SampleDataPoint(void);
-        SampleDataPoint(double data);
+        // Constructor
         SampleDataPoint(int16_t spacecraft, const boost::shared_ptr<Sim42DataPoint> dp);
-
-        // Deconstructors
-        ~SampleDataPoint(void);
 
         // Accessors
         std::string to_string(void) const;
-        double      get_sample_data(void) const {return _sample_data[0];}
+        // vvv These provide the hardware model a way to get the specific data out of the data point that it will need to send
+        // out bytes over its peripheral bus
+        double      get_sample_data_x(void) const {return _sample_data[0];}
+        double      get_sample_data_y(void) const {return _sample_data[1];}
+        double      get_sample_data_z(void) const {return _sample_data[2];}
     
     private:
-        mutable std::vector<double> _sample_data;
+        // Disallow the big 4
+        SampleDataPoint(void) {};
+        SampleDataPoint(const SampleDataPoint&) {};
+        SampleDataPoint& operator=(const SampleDataPoint&) {};
+        ~SampleDataPoint(void) {};
+
+        // vvv This would be the specific data you need to get from the data provider to the hardware model so that
+        // the hardware model can send out bytes over its peripheral bus... but you only get to this data through the accessors above
+        mutable double _sample_data[3];
     };
 }
 
